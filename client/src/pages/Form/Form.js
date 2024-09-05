a0c4e9fe5c88bfa3de6cdd17076e8866034f24be
Nate Miller <nmiller2379@gmail.com>
2024-09-05 06:51:33 -0400

    Styles complete

--- client/src/pages/Form/Form.js
+++ client/src/pages/Form/Form.js
@@ -34,44 +34,51 @@ export default function Form() {
     <div>
       <h1>Submit Your information</h1>
       <form>
-        <label>
-          First Name:
-          <input
-            type="text"
-            name="fname"
-            placeholder="Your first name here..."
-            value={fname}
-            required
-            onChange={(e) => setFname(e.target.value)}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            name="lname"
-            placeholder="Your last name here..."
-            value={lname}
-            required
-            onChange={(e) => setLname(e.target.value)}
-          />
-        </label>
-        <label>
-          Age:
-          <input
-            type="number"
-            name="age"
-            value={age}
-            required
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </label>
+        <div>
+          <label>
+            First Name:
+            <input
+              type="text"
+              name="fname"
+              placeholder="Your first name here..."
+              value={fname}
+              required
+              onChange={(e) => setFname(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              name="lname"
+              placeholder="Your last name here..."
+              value={lname}
+              required
+              onChange={(e) => setLname(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Age:
+            <input
+              type="number"
+              name="age"
+              placeholder="Your last name here..."
+              value={age}
+              required
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </label>
+        </div>
         <button onClick={handleSubmit} type="submit">
           Submit
         </button>
       </form>
       <div>
-        <h2>See Your People</h2>
+        <h2>See Our People</h2>
         <p>Click the button to see a list of all submitted people</p>
         <button onClick={handleClick}>See People</button>
       </div>
